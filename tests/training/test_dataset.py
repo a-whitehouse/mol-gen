d87a68aa0608a56669398d3ffe4b6eeba6135a8e
a-whitehouse <61844534+a-whitehouse@users.noreply.github.com>
2022-05-10 22:59:09 +0100

    Add test dataset module

---
+++ tests/training/test_dataset.py
@@ -0,0 +1,22 @@
+from mol_gen.training.dataset import (
+    add_start_and_end_of_sequence_tokens_to_selfies,
+    get_selfies_string_lookup_layer,
+    split_selfies,
+    split_sequence_to_input_and_target,
+)
+
+
+class TestAddStartAndEndOfSequenceTokensToSelfies:
+    pass
+
+
+class TestGetSelfiesStringLookupLayer:
+    pass
+
+
+class TestSplitSelfies:
+    pass
+
+
+class TestSplitSequenceToInputAndTarget:
+    pass
