c6238c5443095cbb9dde8738f475c97be6ea428f
a-whitehouse <61844534+a-whitehouse@users.noreply.github.com>
2022-05-10 23:01:12 +0100

    Add preprocessed selfies test data

--- tests/training/test_dataset.py
+++ tests/training/test_dataset.py
@@ -1,6 +1,7 @@
 from mol_gen.training.dataset import (
     add_start_and_end_of_sequence_tokens_to_selfies,
     get_selfies_string_lookup_layer,
+    process_selfies_dataset,
     split_selfies,
     split_sequence_to_input_and_target,
 )
@@ -11,6 +12,10 @@ class TestAddStartAndEndOfSequenceTokensToSelfies:
 
 
 class TestGetSelfiesStringLookupLayer:
+    pass
+
+
+class TestProcessSelfiesDataset:
     pass
 
 
