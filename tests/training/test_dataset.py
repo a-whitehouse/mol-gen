b1328c5e25856fa83221450f3a9e0e1980d4f098
a-whitehouse <61844534+a-whitehouse@users.noreply.github.com>
2022-05-11 23:49:16 +0100

    Add unit tests for training dataset module

--- tests/training/test_dataset.py
+++ tests/training/test_dataset.py
@@ -37,10 +37,64 @@ def dataset(tmpdir):
 
 
 @pytest.fixture
-def string_to_integer_layer():
-    return StringLookup(
-        mask_token="[nop]",
-        vocabulary=[
+def vocabulary():
+    return [
+        "[#Branch1]",
+        "[#Branch2]",
+        "[#C]",
+        "[#N]",
+        "[=Branch1]",
+        "[=Branch2]",
+        "[=C]",
+        "[=N]",
+        "[=O]",
+        "[=Ring1]",
+        "[=Ring2]",
+        "[Branch1]",
+        "[Branch2]",
+        "[C]",
+        "[Cl]",
+        "[NH1]",
+        "[N]",
+        "[O]",
+        "[P]",
+        "[Ring1]",
+        "[Ring2]",
+        "[S]",
+    ]
+
+
+@pytest.fixture
+def string_to_integer_layer(vocabulary):
+    return StringLookup(mask_token="[nop]", vocabulary=vocabulary)
+
+
+class TestAddStartAndEndOfSequenceTokensToSelfies:
+    def test_completes_given_valid_input(self):
+        selfies = tf.constant("[C][C][N][Branch1][P][C][=Branch1][C][=O]")
+
+        add_start_and_end_of_sequence_tokens_to_selfies(selfies)
+
+    def test_returns_expected_selfies(self):
+        selfies = tf.constant("[C][C][N][Branch1][P][C][=Branch1][C][=O]")
+
+        actual = add_start_and_end_of_sequence_tokens_to_selfies(selfies)
+
+        assert actual == tf.constant(
+            "[nop][C][C][N][Branch1][P][C][=Branch1][C][=O][nop]"
+        )
+
+
+class TestGetSelfiesStringLookupLayer:
+    def test_completes_given_valid_input(self, vocabulary):
+        get_selfies_string_lookup_layer(vocabulary)
+
+    def test_adds_vocabulary_to_layer_vocabulary(self, vocabulary):
+        layer = get_selfies_string_lookup_layer(vocabulary)
+
+        actual = layer.get_vocabulary()
+
+        assert actual[2:] == [
             "[#Branch1]",
             "[#Branch2]",
             "[#C]",
@@ -63,16 +117,22 @@ def string_to_integer_layer():
             "[Ring1]",
             "[Ring2]",
             "[S]",
-        ],
-    )
+        ]
 
+    def test_adds_mask_token_to_layer_vocabulary(self, vocabulary):
+        layer = get_selfies_string_lookup_layer(vocabulary)
 
-class TestAddStartAndEndOfSequenceTokensToSelfies:
-    pass
+        actual = layer.get_vocabulary()
 
+        assert actual[0] == "[nop]"
 
-class TestGetSelfiesStringLookupLayer:
-    pass
+    def test_ignores_extra_mask_token_in_vocabulary(self, vocabulary):
+        vocabulary.append("[nop]")
+        layer = get_selfies_string_lookup_layer(vocabulary)
+
+        actual = layer.get_vocabulary()
+
+        assert "[nop]" not in actual[1:]
 
 
 class TestProcessSelfiesDataset:
