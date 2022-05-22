70027615b5fb870c2c1583857d6a1e8bb58a8698
Andrew Whitehouse <ajw3789@gmail.com>
2022-05-22 08:27:55 +0100

    Add unit tests for new model config options

--- tests/config/training/test_model.py
+++ tests/config/training/test_model.py
@@ -7,7 +7,17 @@ from mol_gen.exceptions import ConfigException
 class TestModelConfig:
     @pytest.fixture
     def valid_config_section(self):
-        return {"embedding_dim": 64, "lstm_units": 128, "dropout": 0.5}
+        return {
+            "embedding_dim": 64,
+            "lstm_units": 128,
+            "dropout": 0.5,
+            "patience": 2,
+            "epochs": 50,
+        }
+
+    @pytest.fixture
+    def minimal_config_section(self):
+        return {"embedding_dim": 64, "lstm_units": 128}
 
     def test_parse_config_completes_given_valid_config_section(
         self, valid_config_section
@@ -42,10 +52,40 @@ class TestModelConfig:
 
         assert config.dropout == 0.5
 
-    def test_parse_config_sets_expected_dropout_given_none_given(self):
-        config = ModelConfig.parse_config({"embedding_dim": 64, "lstm_units": 128})
+    def test_parse_config_sets_expected_dropout_given_none_given(
+        self, minimal_config_section
+    ):
+        config = ModelConfig.parse_config(minimal_config_section)
 
         assert config.dropout == 0
+
+    def test_parse_config_sets_expected_patience_given_valid_config_section(
+        self, valid_config_section
+    ):
+        config = ModelConfig.parse_config(valid_config_section)
+
+        assert config.patience == 2
+
+    def test_parse_config_sets_expected_patience_given_none_given(
+        self, minimal_config_section
+    ):
+        config = ModelConfig.parse_config(minimal_config_section)
+
+        assert config.patience == 5
+
+    def test_parse_config_sets_expected_epochs_given_valid_config_section(
+        self, valid_config_section
+    ):
+        config = ModelConfig.parse_config(valid_config_section)
+
+        assert config.epochs == 50
+
+    def test_parse_config_sets_expected_epochs_given_none_given(
+        self, minimal_config_section
+    ):
+        config = ModelConfig.parse_config(minimal_config_section)
+
+        assert config.epochs == 100
 
     def test_parse_config_raises_exception_given_embedding_dim_missing(self):
         with pytest.raises(ConfigException):
