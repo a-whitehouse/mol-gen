184691d4afa1883ac52a83d6e1abc139a75ce588
Andrew Whitehouse <ajw3789@gmail.com>
2022-05-22 22:41:49 +0100

    Add molecule generator class

--- src/mol_gen/training/model.py
+++ src/mol_gen/training/model.py
@@ -1,11 +1,17 @@
+from __future__ import annotations
+
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.data import TextLineDataset
 from tensorflow.keras.layers import LSTM, Dense, Embedding, Input
+from tensorflow.python.framework.ops import EagerTensor
 
 from mol_gen.config.training.model import ModelConfig
+from mol_gen.training.dataset import get_selfies_string_lookup_layer
 
 
 def train_model(
@@ -76,3 +82,94 @@ def get_compiled_model(
     )
 
     return model
+
+
+class MoleculeGenerator:
+    def __init__(self, model, string_to_integer_layer, integer_to_string_layer):
+        self.model = model
+
+        self.string_to_integer_layer = string_to_integer_layer
+        self.integer_to_string_layer = integer_to_string_layer
+
+        self.prediction_mask = self._get_prediction_mask(["UNK"])
+        self.prediction_mask_initial = self._get_prediction_mask(
+            [string_to_integer_layer.mask_token]
+        )
+
+    @classmethod
+    def from_files(
+        cls, model_filepath: str, token_counts_filepath: str
+    ) -> MoleculeGenerator:
+        """Loads molecule generator from files.
+
+        Args:
+            model_filepath (str): Path to model checkpoint.
+            token_counts_filepath (str): Path to token counts.
+
+        Returns:
+            MoleculeGenerator: Molecule generator.
+        """
+        model = tf.keras.models.load_model(model_filepath)
+
+        vocabulary = pd.read_csv(token_counts_filepath)["token"].to_list()
+        string_to_integer_layer = get_selfies_string_lookup_layer(vocabulary)
+        integer_to_string_layer = get_selfies_string_lookup_layer(
+            vocabulary, invert=True
+        )
+
+        return cls(model, string_to_integer_layer, integer_to_string_layer)
+
+    def _get_prediction_mask(self, mask_tokens: list[str]) -> EagerTensor:
+        """Gets prediction mask for logits.
+
+        Args:
+            mask_tokens (list[str]): Tokens to mask.
+
+        Returns:
+            EagerTensor: Prediction Mask.
+        """
+        sparse_mask = tf.SparseTensor(
+            values=[-float("inf")] * len(mask_tokens),
+            indices=self.string_to_integer_layer(mask_tokens)[:, None],
+            dense_shape=[self.string_to_integer_layer.vocabulary_size()],
+        )
+        return tf.sparse.to_dense(sparse_mask)
+
+    def generate_molecules(self, n_molecules: int, temperature: float = 1) -> list[str]:
+        """Generates molecules encoded as SELFIES.
+
+        Args:
+            n_molecules (int): Number of molecules to generate.
+            temperature (float, optional): Scaling factor for logits. Defaults to 1.
+
+        Returns:
+            list[str]: Generated molecules.
+        """
+        mols = np.zeros(n_molecules).reshape(-1, 1)
+        complete_mols = np.empty((0, 0))
+
+        while len(mols):
+            pred_logits = self.model.predict(mols)[:, -1, :]
+            pred_logits = (pred_logits / temperature) + self.prediction_mask
+
+            # Don't allow mask token to be applied immediately after starting token
+            if mols.shape[1] == 1:
+                pred_logits += self.prediction_mask_initial
+
+            pred_tokens = tf.random.categorical(pred_logits, 1)
+            mols = np.hstack((mols, pred_tokens))
+
+            new_mols = mols[mols[:, -1] == 0]
+            mols = mols[mols[:, -1] != 0]
+
+            complete_mols = np.pad(
+                complete_mols, ((0, 0), (0, new_mols.shape[1] - complete_mols.shape[1]))
+            )
+
+            if len(new_mols):
+                complete_mols = np.append(complete_mols, new_mols, axis=0)
+
+        mols = self.integer_to_string_layer(complete_mols)
+        mols = tf.strings.reduce_join(mols, axis=1)
+
+        return [i.decode() for i in mols.numpy()[1:]]
